993fff5e67574bfc22f9003f3e3f8103c6b6c14f
lakpa-tamang9 <Ld.tamang25@gmail.com>
2023-04-18 16:12:12 +0900

    feat: Use stable diffusion api from replicate

--- gptapp.py
+++ gptapp.py
@@ -1,48 +1,53 @@
 # For the UI
 from flask import Flask, render_template, request, session
+
 # OpenAI API
 import openai
+
 # Regular expressions:
 import re
 import os
 import replicate
+import sys
 
 # Set the OpenAI API key
 # openai.api_key = open("key.txt", "r").read().strip("\n")
-openai.api_key = "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"
+try:
+    with open("./resources/keys.txt") as f:
+        openai.api_key = f.read()
 
+except KeyError:
+    print("Cannot find API key")
+    sys.exit()
 # Create a new Flask app and set the secret key
 app = Flask(__name__)
 app.secret_key = "mysecretkey"
 
+
 # Define a function to generate an image using the OpenAI API
 def get_img(prompt):
     try:
-        output = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-                               input = prompt)
-        response = openai.Image.create(
-            prompt=prompt,
-            n=1,
-            size="512x512"
-            )
-        print(output["items"]["format"])
+        output = replicate.run(
+            "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+            input=prompt,
+        )
+        # response = openai.Image.create(prompt=prompt, n=1, size="512x512")
         # img_url = response.data[0].url
+        img_url = output[0]
     except Exception as e:
         # if it fails (e.g. if the API detects an unsafe image), use a default image
         img_url = "https://pythonprogramming.net/static/images/imgfailure.png"
-    return output["items"]["format"]
+    return img_url
 
 
 # Define a function to generate a chat response using the OpenAI API
 def chat(inp, message_history, role="user"):
-
     # Append the input message to the message history
     message_history.append({"role": role, "content": f"{inp}"})
 
     # Generate a chat response using the OpenAI API
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=message_history
+        model="gpt-3.5-turbo", messages=message_history
     )
 
     # Grab just the text from the API completion response
@@ -56,28 +61,35 @@ def chat(inp, message_history, role="user"):
 
 
 # Define the homepage route for the Flask app
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
     # Page's title:
     title = "GPT-Journey"
-    
+
     # Initialize the button messages and button states dictionaries
     button_messages = {}
     button_states = {}
 
     # If the request method is GET (i.e., the page has just been loaded), set up the initial chat
-    if request.method == 'GET':
-
+    if request.method == "GET":
         # Initialize the message history
-        session['message_history'] = [{"role": "user", "content": """You are an interactive story game bot that proposes some hypothetical fantastical situation where the user needs to pick from 2-4 options that you provide. Once the user picks one of those options, you will then state what happens next and present new options, and this then repeats. If you understand, say, OK, and begin when I say "begin." When you present the story and options, present just the story and start immediately with the story, no further commentary, and then options like "Option 1:" "Option 2:" ...etc."""},
-                                      {"role": "assistant", "content": f"""OK, I understand. Begin when you're ready."""}]
-        
+        session["message_history"] = [
+            {
+                "role": "user",
+                "content": """You are an interactive story game bot that proposes some hypothetical fantastical situation where the user needs to pick from 2-4 options that you provide. Once the user picks one of those options, you will then state what happens next and present new options, and this then repeats. If you understand, say, OK, and begin when I say "begin." When you present the story and options, present just the story and start immediately with the story, no further commentary, and then options like "Option 1:" "Option 2:" ...etc.""",
+            },
+            {
+                "role": "assistant",
+                "content": f"""OK, I understand. Begin when you're ready.""",
+            },
+        ]
+
         # Retrieve the message history from the session
-        message_history = session['message_history']
+        message_history = session["message_history"]
 
         # Generate a chat response with an initial message ("Begin")
         reply_content, message_history = chat("Begin", message_history)
-        
+
         # Extract the text from the response
         text = reply_content.split("Option 1")[0]
 
@@ -92,18 +104,16 @@ def home():
         for button_name in button_messages.keys():
             button_states[button_name] = False
 
-
     # If the request method is POST (i.e., a button has been clicked), update the chat
     message = None
     button_name = None
-    if request.method == 'POST':
-
+    if request.method == "POST":
         # Retrieve the message history and button messages from the session
-        message_history = session['message_history']
-        button_messages = session['button_messages']
+        message_history = session["message_history"]
+        button_messages = session["button_messages"]
 
         # Get the name of the button that was clicked  ***
-        button_name = request.form.get('button_name')
+        button_name = request.form.get("button_name")
 
         # Set the state of the button to "True"
         button_states[button_name] = True
@@ -126,15 +136,26 @@ def home():
             button_states[button_name] = False
 
     # Store the updated message history and button messages in the session
-    session['message_history'] = message_history
-    session['button_messages'] = button_messages
+    session["message_history"] = message_history
+    session["button_messages"] = button_messages
 
-    # Generate an image based on the chat response text    
-    image_url = get_img(text)
+    pmpt = {"prompt": text}
+    # Generate an image based on the chat response text
+    image_url = get_img(pmpt)
+    print(image_url)
 
     # Render the template with the updated information
-    return render_template('my.html', title=title, text=text, image_url=image_url, button_messages=button_messages, button_states=button_states, message=message)
+    return render_template(
+        "my.html",
+        title=title,
+        text=text,
+        image_url=image_url,
+        button_messages=button_messages,
+        button_states=button_states,
+        message=message,
+    )
+
 
 # Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True, port=5003)
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
